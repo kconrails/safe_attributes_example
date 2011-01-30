3e4aaa1c985eabf8affd95f3c921b0c12c061777
Jaime Bellmyer <online@bellmyer.com>
2011-01-30 09:28:43 -0600

    added accessors, and a validation based on them

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
   bad_attribute_names :class
+  
+  validates_presence_of :class_name
+  
+  def class_name= value
+    self[:class] = value
+  end
+  
+  def class_name
+    self[:class]
+  end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,33 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "class_name writes to class attribute" do
+    u = User.new
+    
+    u.class_name = 'class of 1995'
+    assert_equal 'class of 1995', u[:class]
+  end
+  
+  test "class_name reads from class attribute" do
+    u = User.new
+    
+    u[:class] = 'class of 1995'
+    assert_equal 'class of 1995', u.class_name
+  end
+  
+  test "validates presence of class_name" do
+    error_message = "can't be blank"
+    
+    u = User.new :class_name => 'class of 1995'
+    u.valid?
+    assert !Array(u.errors[:class_name]).include?(error_message)
+    
+    u.class_name = nil
+    assert !u.valid?
+    assert Array(u.errors[:class_name]).include?(error_message)
+    
+    u.class_name = ''
+    assert !u.valid?
+    assert Array(u.errors[:class_name]).include?(error_message)
   end
 end
